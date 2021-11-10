04758d54de81065ed185556c0f3cebb65f1e4947
LavanyaInbakumar <82710203+LavanyaInbakumar@users.noreply.github.com>
2021-11-10 18:50:39 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import Register from './android/app/src/Pages/register'
+import Login from './android/app/src/Pages/login'
+
+
+const App = () => {
+  return (
+    <Login/>
+    // <Register/>
+  );
+};
+
+export default App;
