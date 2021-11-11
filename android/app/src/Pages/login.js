7bff5d4121addeb50ce24cc8d409a4f147e05bf4
Paladin <paladinsoftsol@gmail.com>
2021-11-11 21:25:56 +0530

    login

--- android/app/src/Pages/login.js
+++ android/app/src/Pages/login.js
@@ -1,9 +1,11 @@
 import React, { useState, useRef } from 'react';
-import { Text, View, TouchableOpacity, SafeAreaView, StyleSheet, Image, StatusBar } from 'react-native'
+import { Text, View, TouchableOpacity, SafeAreaView, StyleSheet, Image, StatusBar, TextInput, ScrollView } from 'react-native'
 import PhoneInput from 'react-native-phone-number-input';
 import DropDownPicker from 'react-native-dropdown-picker';
-// import Icon from 'react-native-vector-icons/FontAwesome';
-// import { Input } from 'react-native-elements';
+// import Icon from 'react-native-vector-icons';
+import { Input, Icon } from 'react-native-elements';
+// import Icon from 'react-native-ico-font-awesome';
+
 
 const Login = () => {
     const [phoneValue, setPhoneValue] = useState(null)
@@ -11,22 +13,24 @@ const Login = () => {
     const phoneRef = useRef(undefined);
     const [open, setOpen] = useState(false);
     const [value, setValue] = useState(null);
+    const [input, setInput] = useState('')
     const [items, setItems] = useState([
+
         { label: 'Apple', value: 'apple' },
         { label: 'Banana', value: 'banana' }
     ]);
     return (
         <SafeAreaView style={styles.outer}>
             <StatusBar barStyle='light-content' backgroundColor='#FFBF00' hidden={true} />
-            <View style={styles.container}>
-                <View style={{ width: '100%', justifyContent: 'center', alignItems: 'center' }}>
+            <ScrollView style={styles.container}>
+                <View style={styles.logoContainer}>
                     <Image source={require('../images/logo.png')} style={styles.logo} />
                 </View>
-                <View style={{ width: '100%', justifyContent: 'center', alignItems: 'center', marginTop: 30 }}>
+                <View style={styles.loginContainer}>
                     <Image source={require('../images/mobile.png')} style={styles.mobile} />
                     <Text style={styles.Text}>Login/SignUp</Text>
                 </View>
-                <View style={{ width: '100%', justifyContent: 'center', alignItems: 'center', marginTop: 30 }}>
+                <View style={styles.midContainer}>
                     <PhoneInput
                         containerStyle={styles.phoneInput}
                         ref={phoneRef}
@@ -36,15 +40,25 @@ const Login = () => {
                             withShadow = true
                         }}
                     />
+                    <View style={styles.inputContainer}>
+                        <Image source={require("../images/mobile_icon.png")} style={styles.mobileIcon} />
+                        <TextInput
+                            placeholder="Phone Number"
+                            style={styles.textInput}
+                            onChangeText={value => setValue(value)}
+                        />
+
+                    </View>
                     {/* <Input
                         placeholder='INPUT WITH CUSTOM ICON'
                         leftIcon={
                             <Icon
-                                name='fa-solid fa-mobile-screen'
-                                size={24}
-                                color='black'
+                                name='sc-telegram'
+                                type='evilicon'
+                                color='#517fa4'
                             />
                         }
+                        containerStyle={styles.textInput}
                     /> */}
                     {/* <DropDownPicker
                         open={open}
@@ -59,30 +73,32 @@ const Login = () => {
                         <Text style={styles.btnText}>SUBMIT</Text>
                     </TouchableOpacity>
                 </View>
-                <View style={{ width: '100%', justifyContent: 'center', alignItems: 'center', marginTop: 30, position: 'absolute', bottom: 0, padding: 10 }}>
+                <View style={styles.termContainer}>
                     <Text style={styles.terms}>By signing up, you agree to Dealynx’s <Text style={styles.span}>{"Terms & Conditions"}</Text></Text>
                     <Text style={styles.terms}>and <Text style={styles.span}>{"Privacy Policy"}</Text></Text>
                 </View>
-            </View>
+            </ScrollView>
         </SafeAreaView>
     );
 }
 const styles = StyleSheet.create({
     outer: {
         flex: 1,
-        height: '100%',
         width: '100%',
+        height: '100%',
         padding: 30,
         backgroundColor: 'white',
     },
     container: {
         flex: 1,
-        height: '100%',
         width: '100%',
-        backgroundColor: 'transparent',
+        height: '100%',
         paddingTop: 20,
-        position: 'relative',
     },
+    logoContainer: { width: '100%', justifyContent: 'center', alignItems: 'center' },
+    loginContainer: { width: '100%', justifyContent: 'center', alignItems: 'center', marginTop: 30 },
+    midContainer: { width: '100%', justifyContent: 'center', alignItems: 'center', marginTop: 30 },
+    termContainer: { width: '100%', justifyContent: 'flex-end', alignItems: 'center', height: 120 },
     logo: {
         width: '100%',
         height: 60
@@ -135,7 +151,38 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.5,
         shadowRadius: 5,
         elevation: 5,
-        margin: 10
+        margin: 10,
+        height: 70,
+    },
+    mobileIcon: {
+        width: '10%',
+        height: '50%',
+        resizeMode: 'contain',
+        marginLeft: 12
+    },
+    inputContainer: {
+        width: '95%',
+        height: 70,
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#fff',
+        shadowColor: '#000',
+        shadowOffset: { width: 1, height: 1 },
+        shadowOpacity:  0.4,
+        shadowRadius: 3,
+        elevation: 5,
+        margin:10
+    },
+    textInput: {
+        width: '86%',
+        height: '80%',
+        padding: 5,
+        borderWidth: 0,
+        fontSize: 18,
+        alignContent: 'center',
+        paddingLeft: 10
     }
 })
 export default Login;
